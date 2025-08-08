7de8067aa70867cc2248967df95a08f58eb4d965
ayaz <ayaz17sheikh@gmail.com>
2025-08-08 20:03:16 +0530

    add some more oops concept

--- oop/class.py
+++ oop/class.py
@@ -3,10 +3,10 @@ class Car:
 
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
         Car.total_car+=1
     def fullname(self):
-        return f"{self.__brand} {self.model}"    
+        return f"{self.__brand} {self.__model}"    
     def get_brand(self):
         return self.__brand
     def fuel_type(self):
@@ -14,6 +14,10 @@ class Car:
     @staticmethod
     def general_description():
         return "Cars are means of transport"
+    
+    @property
+    def model(self):
+        return self.__model
     
 
 class ElectricCar(Car):
@@ -23,16 +27,35 @@ class ElectricCar(Car):
     def fuel_type(self):
         return "Electric charge"
 my_car = Car("toyota", "corolla")
-print(my_car.get_brand)
+# my_car.model = "city"
+# print(my_car.get_brand)
+# print(my_car.model)
+# print(my_car.fullname())
+# print(my_car.fuel_type())
 print(my_car.model)
-print(my_car.fullname())
-print(my_car.fuel_type())
-my_electric_car = ElectricCar("tesla", "cybertruck","85 kWh")
+my_tesla = ElectricCar("tesla", "cybertruck","85 kWh")
+print(isinstance(my_tesla,Car))
+print(isinstance(my_tesla,ElectricCar))
 # print(my_electric_car.battery_size)
 # print(my_electric_car.brand)
 # print(my_electric_car.model)
-print(my_electric_car.fullname())
-print(my_electric_car.fuel_type())
-print(Car.total_car)
-print(Car.general_description())
+# print(my_electric_car.fullname())
+# print(my_electric_car.fuel_type())
+# print(Car.total_car)
+# print(Car.general_description())
 # print(my_car.general_description())
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+
+class Engine:
+    def engine_info(self):
+        return "this is engine"
+
+class ElectricCar2(Battery,Engine,Car):
+    pass
+
+my_new_tesla = ElectricCar2("tesla", "model s")
+print(my_new_tesla.battery_info())
+print(my_new_tesla.engine_info())
